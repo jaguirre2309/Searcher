0473427dc96113549e6e4b27ac58159c7314382a
jaguirre2309 <33992318+jaguirre2309@users.noreply.github.com>
2019-02-20 23:54:26 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@
   /*
     Función que reproduce el video de fondo al hacer scroll, y deteiene la reproducción al detener el scroll
   */
-  /*function playVideoOnScroll(){
+  function playVideoOnScroll(){
     var ultimoScroll = 0,
         intervalRewind;
     var video = document.getElementById('vidFondo');
@@ -52,7 +52,7 @@
       .scrollEnd(()=>{
         video.pause();
       }, 10)
-  }*/
+  }
 
   inicializarSlider();
   //playVideoOnScroll();
@@ -79,19 +79,19 @@
         for (let item of datos) {
           /*console.log(item);*/
           res.innerHTML += `
-            <div class="col s3 m3">
+            <div class="col s3 m3 l3">
               <div class="card horizontal">
                 <div class="card-image" >
-                  <img src="img/home.jpg">
+                  <img src="img/home.jpg" style="width:35% !important;">
                 </div>
-                <div class="card-stacked">
+                <div class="">
                   <div class="card-content">
                     <p><strong>Dirección:</strong> ${item.Direccion}</p>
                     <p><strong>Ciudad:</strong> ${item.Ciudad}</p>
                     <p><strong>Telefono:</strong> ${item.Telefono}</p>
                     <p><strong>Códigp Postal:</strong> ${item.Codigo_Postal}</p>
                     <p><strong>Tipo:</strong> ${item.Tipo}</p>
-                    <p><strong>Precio:</strong> ${item.Precio}</p>
+                    <p><strong>Precio:</strong></p><p style="color: #ffcc00; font-size: 1.5rem;"><strong> ${item.Precio}</strong></p>
                   </div>
                   <div class="card-action">
                     <a href="#">Ver Mas</a>
@@ -124,8 +124,8 @@
         let resTipo = document.querySelector('#selectTipo');
         resCiudad.innerHTML = '';
         resTipo.innerHTML = '';
-        resCiudad.innerHTML = '<option value="" selected>Seleccione una Ciudad</option>';
-        resTipo.innerHTML = '<option value="" selected>Seleccione un Tipo</option>';
+        resCiudad.innerHTML = '<option value="0" selected>Seleccione una Ciudad</option>';
+        resTipo.innerHTML = '<option value="0" selected>Seleccione un Tipo</option>';
 
         function onlyUnique(value, index, self) { 
           return self.indexOf(value) === index;
@@ -176,17 +176,19 @@
  
 llenarCombos();
 
-//Boton buscar por precio, ciudad o tipo
+//Ejecuta funcion Buscar al hacer clic en boton "Buscar"
 document.querySelector('#submitButton').addEventListener('onclick',buscar);
 
-
+//Funcíon de boton buscar
 function buscar(){
     //console.log('Dentro de la función');
     var precio = document.querySelector('#rangoPrecio').value;
     var ciudadJSON = document.querySelector('#selectCiudad').value;
     var tipoJSON = document.querySelector('#selectTipo').value;
-    var ran = precio.split(";",2)
-    console.log(ran[0],ran[1],ciudadJSON,tipoJSON);
+    var ran = precio.split(";",2);
+    let min = parseInt(ran[0]);
+    let max = parseInt(ran[1]);
+    //console.log(min,max,ciudadJSON,tipoJSON + 'Variables');
 
     ciudad = ciudadJSON;
     tipo = tipoJSON;
@@ -200,27 +202,69 @@ function buscar(){
         let datos2 = JSON.parse(this.responseText);
         //console.log(datos2);
 
-        
-        
+        //Obtiene información del JSON segun los datos de ciudad, tipo y precio
+        if (ciudadJSON == '0' && tipoJSON == '0') {
           var prueba = datos2.filter(function(index) {
+            //console.log(index.Precio);
+            var ran1 = index.Precio.replace("$","");
+            var sinComa1 = ran1.replace(",","");
+            var precioJSON = parseInt(sinComa1);
+            //console.log(precioJSON);
             //index equivalente a cada elemento del objeto datos2
-            return index.Ciudad === ciudadJSON && index.Tipo === tipoJSON && index.Precio >= ran;
-            console.log(prueba);
+            return precioJSON >= min && precioJSON <= max;
+            //console.log(prueba,+' Filtro');
           });
-        
+        }else if (tipoJSON == '0') {
+          //Realiza filtro segun parametros
+          var prueba = datos2.filter(function(index) {
+            //console.log(index.Precio);
+            var ran1 = index.Precio.replace("$","");
+            var sinComa1 = ran1.replace(",","");
+            var precioJSON = parseInt(sinComa1);
+            //console.log(precioJSON);
 
-        
+            //index equivalente a cada elemento del objeto datos2
+            return precioJSON >= min && precioJSON <= max && index.Ciudad === ciudadJSON;
+            //console.log(prueba,+' Filtro');
+          });
+        }else if(ciudadJSON = '0'){
+          var prueba = datos2.filter(function(index) {
+            //console.log(index.Precio);
+            var ran1 = index.Precio.replace("$","");
+            var sinComa1 = ran1.replace(",","");
+            var precioJSON = parseInt(sinComa1);
+            //console.log(precioJSON);
+
+            //index equivalente a cada elemento del objeto datos2
+            return precioJSON >= min && precioJSON <= max && index.Tipo === tipoJSON;
+            //console.log(prueba,+' Filtro');
+          });
+        }else {
+          var prueba = datos2.filter(function(index) {
+            //console.log(index.Precio);
+            var ran1 = index.Precio.replace("$","");
+            var sinComa1 = ran1.replace(",","");
+            var precioJSON = parseInt(sinComa1);
+            //console.log(precioJSON);
+
+            //index equivalente a cada elemento del objeto datos2
+            return precioJSON >= min && precioJSON <= max && index.Ciudad === ciudadJSON && index.Tipo === tipoJSON;
+            //console.log(prueba,+' Filtro');
+          });
+        }
+
 
         let res2 = document.querySelector('#datos');
         res2.innerHTML = '';
 
+        //Recorre arregle de datos para mostrarlos en el HTML
         for (let item2 of prueba) {
           /*console.log(item);*/
           res2.innerHTML += `
-            <div class="col s3 m3">
+            <div class="col s3 m3 l3">
               <div class="card horizontal">
                 <div class="card-image" >
-                  <img src="img/home.jpg">
+                  <img src="img/home.jpg" style="width:35% !important;">
                 </div>
                 <div class="card-stacked">
                   <div class="card-content">
@@ -229,7 +273,7 @@ function buscar(){
                     <p><strong>Telefono:</strong> ${item2.Telefono}</p>
                     <p><strong>Códigp Postal:</strong> ${item2.Codigo_Postal}</p>
                     <p><strong>Tipo:</strong> ${item2.Tipo}</p>
-                    <p><strong>Precio:</strong> ${item2.Precio}</p>
+                    <p><strong>Precio:</strong></p><p style="color: #ffcc00; font-size: 1.5rem;"><strong> ${item2.Precio}</strong></p>
                   </div>
                   <div class="card-action">
                     <a href="#">Ver Mas</a>
